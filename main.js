bb1c38e1d1225eb8d1f3ef010e55fcc835228302
dnyaneshwarmagar <dnyaneshwarmagar276@gmail.com>
2024-05-21 05:19:02 +0530

    done

--- main.js
+++ main.js
@@ -3,10 +3,17 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   // TODO: Send the ""Hello, world!"" response
+
+  // Set the response HTTP header with HTTP status and Content type
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  // Send the response body "Hello, world!"
+  res.end('Hello, world!');
+  
 });
 
 server.listen(port, () => {
   // TODO: Log a message to the console when the server starts listening on the port
+  console.log(`Server is listening on port ${port}`);
 });
 
 // DO NOT EDIT BELOW THIS LINE
